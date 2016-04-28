4b67fad3653fe0fbe9c0f43972224c278760d0dc
Sean Shubin <sshubin@cj.com>
2016-04-27 17:26:01 -0700

    Rearranged code

--- console/src/main/scala/com/seanshubin/todo/application/console/ConfigurationWiring.scala
+++ console/src/main/scala/com/seanshubin/todo/application/console/ConfigurationWiring.scala
@@ -24,10 +24,10 @@ trait ConfigurationWiring {
   )
   lazy val classLoader: ClassLoader = getClass.getClassLoader
   lazy val classLoaderContract: ClassLoaderContract = new ClassLoaderDelegate(classLoader)
-  lazy val classPathHandler: ValueHandler = new ClassPathHandler(serveFromClasspathPrefix, classLoaderContract, contentTypeByExtension)
   lazy val files: FilesContract = FilesDelegate
   lazy val redirectHandler: ValueHandler = new RedirectHandler(redirects)
   lazy val fileSystemHandler: ValueHandler = new FileSystemHandler(serveFromDirectory, files, contentTypeByExtension)
+  lazy val classPathHandler: ValueHandler = new ClassPathHandler(serveFromClasspathPrefix, classLoaderContract, contentTypeByExtension)
   lazy val compositeHandler: ValueHandler = new CompositeHandler(redirectHandler, fileSystemHandler, classPathHandler)
   lazy val handler: Handler = new HandlerAdapter(compositeHandler)
   lazy val runner: Runnable = new JettyRunner(JettyServerDelegate.create, handler, configuration.port)
