fbf391643d807da48f2124edcb57832e6563d441
Sean Shubin <sshubin@cj.com>
2016-04-28 17:37:07 -0700

    messing with tests

--- core/src/main/resources/serve-from-classpath/tests.js
+++ core/src/main/resources/serve-from-classpath/tests.js
@@ -1,7 +1,26 @@
-define(['qunit'],
-    function (qunit) {
+define(['qunit', 'tasks', 'fake-http'],
+    function (qunit, createTasks, fakeHttp) {
         'use strict';
-        qunit.test("tasks", function (assert) {
+
+        qunit.test("render tasks", function (assert) {
+            var tasks = createTasks();
+            tasks.setHttp(fakeHttp);
+
+            fakeHttp.addRequestResponsePair(
+                {
+                    request: {
+                        url: 'database/task',
+                        method: 'GET'
+                    },
+                    response: {
+                        status: 200,
+                        body: '1 false Task A\n2 true Task B\n3 false Task C'
+                    }
+                }
+            );
+
+            var dom = tasks.render();
+
             assert.ok(1 == "1", "Passed!");
         });
     }
